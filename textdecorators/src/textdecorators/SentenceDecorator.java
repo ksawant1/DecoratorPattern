e0839627feaeac0346370ab718ebce67c3b95ba8
ksawant1 <54869724+ksawant1@users.noreply.github.com>
2020-08-03 07:01:52 +0530

    added a sentence decorator that adds a string at start and end of sentence

---
+++ textdecorators/src/textdecorators/SentenceDecorator.java
@@ -0,0 +1,17 @@
+package textdecorators;
+import textdecorators.AbstractTextDecorator;
+import  textdecorators.util.InputDetails;
+
+public class SentenceDecorator extends AbstractTextDecorator{
+	private AbstractTextDecorator atd;
+	private InputDetails id;
+
+	public SentenceDecorator(AbstractTextDecorator atdIn, InputDetails idIn) {
+		atd = atdIn;
+		id = idIn;
+	}
+public void processInputDetails(){
+
+		}
+
+		}
